5c82b711111269c70a62043cfc7083f01e1a6fb4
pikachuface <filipgajdusek1@gmail.com>
2020-03-09 12:42:01 +0100

    Build files

---
+++ Windows/BuildGCC.bat
@@ -0,0 +1,6 @@
+
+echo building your app...
+cd ..\ 
+Cmake -G "MinGW Makefiles" .\ConsoleGameCpp\
+Cmake --build .
+pause
---
+++ Windows/MakeSln.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Making solution...
+cd ../
+Cmake -G "Visual Studio 16 2019" .
