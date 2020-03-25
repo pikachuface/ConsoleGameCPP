4c744170c3243dcf51f445d806328763daf9ac4d
N/A <pikachuface@PikachuFace-Kali.pikachuface_kali.net>
2020-03-25 15:47:19 +0100

    Scripts for windows

--- Windows/BuildGCC.bat
+++ Windows/BuildGCC.bat
@@ -1,6 +1,15 @@
 
-echo building your app...
-cd ..\ 
-Cmake -G "MinGW Makefiles" .\ConsoleGameCpp\
-Cmake --build .
+echo Making gcc make files....
+pushd ../
+
+set builddir=Build
+set rootdir=%cd%
+
+md %builddir%
+
+pushd %builddir%
+cmake -G "MinGW Makefiles" %rootdir%
+echo 
+cmake --build . --config Debug
+echo Building your app.....
 pause
--- Windows/MakeSln.bat
+++ Windows/MakeSln.bat
@@ -1,4 +1,12 @@
 @echo off
-echo Making solution...
-cd ../
-Cmake -G "Visual Studio 16 2019" .
+echo Making your VS solution...
+
+pushd ../
+
+set builddir=Build
+set rootdir=%cd%
+
+md %builddir%
+
+pushd %builddir%
+Cmake -G "Visual Studio 16 2019" %rootdir%
